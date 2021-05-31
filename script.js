03dc5dcaa8470a84c4ae90e22fd1c3cbd39a5750
unknown <lamichhanerasik2020@gmail.com>
2021-05-31 09:05:41 +0545

    All completed and sitereport left

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const toggleButton = document.getElementsByClassName('navigation_menu')[0]
-const navbarLinks = document.getElementsByClassName('button_hover')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
